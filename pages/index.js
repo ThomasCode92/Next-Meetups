ae4db79a8005cd4ebe877aaad9be0216384f7ff1
ThomasCode92 <thomascode92@hotmail.com>
2023-04-25 19:20:43 +0200

    Outputting a List of Meetups

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
+import MeetupList from '@components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image: 'https://frantic.im/assets/react-conf-2018/og-image.jpg',
+    address: 'Some address 5, 12345 Some City',
+    description: 'This is a first meetup!',
+  },
+  {
+    id: 'm2',
+    title: 'A Second Meetup',
+    image: 'https://frantic.im/assets/react-conf-2018/og-image.jpg',
+    address: 'Some address 10, 12345 Some City',
+    description: 'This is a second meetup!',
+  },
+];
+
 function HomePage() {
-  return <h1>Home Page</h1>;
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
 
 export default HomePage;
