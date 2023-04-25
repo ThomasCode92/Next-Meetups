c3346dc4b52972bf0eb0d091a34ddc27314fa2c5
ThomasCode92 <thomascode92@hotmail.com>
2023-04-25 09:00:55 +0200

    Preparing the Project Pages

---
+++ pages/meetup/[meetupId].js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MeetupDetailPage() {
+  return <h1>Meetup Details Page</h1>;
+}
+
+export default MeetupDetailPage;
---
+++ pages/meetup/new.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AddMeetupPage() {
+  return <h1>Add New Meetup</h1>;
+}
+
+export default AddMeetupPage;
