e0ec5293248379e9bca7bf3e50901244efc261ee
ThomasCode92 <thomascode92@hotmail.com>
2023-04-26 12:50:19 +0200

    The "_app.js" File & Layout Wrapper

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Link from 'next/link';
+
 import classes from './MainNavigation.module.css';
 
 function MainNavigation() {
@@ -8,10 +10,10 @@ function MainNavigation() {
       <nav>
         <ul>
           <li>
-            <Link to="/">All Meetups</Link>
+            <Link href="/">All Meetups</Link>
           </li>
           <li>
-            <Link to="/new-meetup">Add New Meetup</Link>
+            <Link href="/meetup/new">Add New Meetup</Link>
           </li>
         </ul>
       </nav>
