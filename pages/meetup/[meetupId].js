55c64cda7f173daab4e8a1ec6d7e05696e253aa9
ThomasCode92 <thomascode92@hotmail.com>
2023-04-27 13:15:23 +0200

    Adding Custom Components & CSS Modules

--- pages/meetup/[meetupId].js
+++ pages/meetup/[meetupId].js
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import MeetupDetail from '@components/meetups/MeetupDetail';
+
 function MeetupDetailPage() {
-  return <h1>Meetup Details Page</h1>;
+  return (
+    <MeetupDetail
+      image="https://frantic.im/assets/react-conf-2018/og-image.jpg"
+      title="First Meetup"
+      address="Some Street 5, Some City"
+      description="This is a first meetup"
+    />
+  );
 }
 
 export default MeetupDetailPage;
