2759ec3c566d6b39aef4a990ff18c80863bacc61
ThomasCode92 <thomascode92@hotmail.com>
2023-04-29 14:01:10 +0200

    Working with Params for SSG Data Fetching

--- pages/meetup/[meetupId].js
+++ pages/meetup/[meetupId].js
@@ -13,4 +13,23 @@ function MeetupDetailPage() {
   );
 }
 
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+
+  // fetch data for a single meetup
+
+  return {
+    props: {
+      meetupData: {
+        id: 'm1',
+        image: 'https://frantic.im/assets/react-conf-2018/og-image.jpg',
+        title: 'First Meetup',
+        address: 'Some Street 5, Some City',
+        description: 'This is a first meetup',
+      },
+    },
+  };
+}
+
 export default MeetupDetailPage;
