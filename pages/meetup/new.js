12b50dc85e2f57f9c1210129fdf72028bdd0215a
ThomasCode92 <thomascode92@hotmail.com>
2023-05-01 11:24:35 +0200

    Sending Http Requests to API Routes

--- pages/meetup/new.js
+++ pages/meetup/new.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
 import NewMeetupForm from '@components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
-  const addMeetupHandler = enteredMeetupData => {
-    console.log(enteredMeetupData);
+  const router = useRouter();
+
+  const addMeetupHandler = async enteredMeetupData => {
+    const response = await fetch('/api/meetup/new', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(enteredMeetupData),
+    });
+
+    const data = await response.json();
+    console.log(data);
+
+    router.push('/');
   };
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
