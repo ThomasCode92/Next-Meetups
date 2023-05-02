5fa99b60b0bb6cd8201d91aeed190084097da648
ThomasCode92 <thomascode92@hotmail.com>
2023-05-02 21:12:14 +0200

    Getting Data from the Database

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,31 @@
 import React from 'react';
+import { MongoClient } from 'mongodb';
 
 import MeetupList from '@components/meetups/MeetupList';
-
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'A First Meetup',
-    image: 'https://frantic.im/assets/react-conf-2018/og-image.jpg',
-    address: 'Some address 5, 12345 Some City',
-    description: 'This is a first meetup!',
-  },
-  {
-    id: 'm2',
-    title: 'A Second Meetup',
-    image: 'https://frantic.im/assets/react-conf-2018/og-image.jpg',
-    address: 'Some address 10, 12345 Some City',
-    description: 'This is a second meetup!',
-  },
-];
 
 function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
 
 export async function getStaticProps() {
-  // fetch data from an API
+  const mongodbUrl = 'mongodb://localhost:27017/next-meetups';
+  const client = await MongoClient.connect(mongodbUrl);
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
   return {
-    props: { meetups: DUMMY_MEETUPS },
+    props: {
+      meetups: meetups.map(meetup => ({
+        id: meetup._id.toString(),
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+      })),
+    },
   };
 }
 
